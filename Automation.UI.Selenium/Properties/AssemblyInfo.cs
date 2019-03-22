78f2cd36f52651ab04c99cde0617bb73c8c3c46d
Talha Liaquat <talha.liaquat@gmail.com>
2019-03-22 23:51:30 +0400

    Project Initialization

---
+++ Automation.UI.Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Automation.UI.Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Automation.UI.Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("973a61b3-685a-4a87-a3b0-ddbc1231ace9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Automation.UI.Selenium/SampleUITest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Automation.UI.Selenium
+{
+    [TestClass]
+    public class SampleUITest
+    {
+        [TestMethod]
+        public void TestNasdaq_SharesHasValue_Valid()
+        {
+
+        }
+    }
+}
